5d853d70c88cb941c590bb7e3789e9cdf201a12e
Ren Wang <ren.wang1202@gmail.com>
2024-09-26 20:21:06 +1000

    feat: add contact us page

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 app.get('/', function(req, res){
     res.send('Hello world')
 })
+
+app.get('/contact', function(req, res){
+    res.send('Contact Us')
+})
+
 app.listen(8080, function() {
     console.log('Listening at port 8080.')
 })
